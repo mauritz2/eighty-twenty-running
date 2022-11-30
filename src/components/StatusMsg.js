5336a99400e24575847afe74c8efc15fe76f2d58
unknown <12560602+mauritz2@users.noreply.github.com>
2022-11-30 14:25:22 +0100

    Goal col hides when no goal is set

--- src/components/StatusMsg.js
+++ src/components/StatusMsg.js
@@ -1,15 +1,15 @@
 import React from "react"
 
 const StatusMsg = ({planName, currentWeek, goal}) => {
-    //             {goal ? <p>Goal: {goal}</p>:""}
+    //             
     return(
         <div id="plan-status">
             <div className="msg-header">Plan</div>
             <div className="msg-header">Week</div>
-            <div className="msg-header">Goal</div>
+            <div className="msg-header">{goal ?"Goal":""}</div>
             <div className="msg-data">{planName}</div>
             <div className="msg-data">{currentWeek}</div>
-            <div className="msg-data">{goal}</div>
+            <div className="msg-data">{goal ? goal:""}</div>
         </div>
     )
 }
