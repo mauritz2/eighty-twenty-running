b298e7f423dd58921b7690796dfadf682500d98c
unknown <12560602+mauritz2@users.noreply.github.com>
2022-11-30 14:08:00 +0100

    Fixed accordion transition expansion

--- src/components/Week.js
+++ src/components/Week.js
@@ -12,19 +12,21 @@ const Week = ({workouts, weekNum}) => {
     console.log("WeekNum");
     console.log(weekNum);
 
+    /*
+    <div className="table-heading">Mon</div>
+    <div className="table-heading">Tue</div>
+    <div className="table-heading">Wed</div>
+    <div className="table-heading">Thu</div>
+    <div className="table-heading">Fri</div>
+    <div className="table-heading">Sat</div>
+    <div className="table-heading">Sun</div>
+    */
+
     return(
         <>
         <button className={open ? "accordion active":"accordion"} onClick={toggleVisibility}>Week {weekNum}</button>
         <div className={open ? "panel block":"panel none"}>
-            <div id="workout-schedule-container">
-                    <div className="table-heading">Mon</div>
-                    <div className="table-heading">Tue</div>
-                    <div className="table-heading">Wed</div>
-                    <div className="table-heading">Thu</div>
-                    <div className="table-heading">Fri</div>
-                    <div className="table-heading">Sat</div>
-                    <div className="table-heading">Sun</div>
-            
+            <div id="workout-schedule-container"> 
                     {workouts.map((workout) => ( 
                         <WorkoutCard
                             key={workout.id}
