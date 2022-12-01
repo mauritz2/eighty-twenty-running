166ea761570b341736ab7840e0c570e286321ce0
unknown <12560602+mauritz2@users.noreply.github.com>
2022-12-01 18:34:49 +0100

    Cleaned up WorkoutCard

--- src/components/current-plan-screen/WorkoutCard.js
+++ src/components/current-plan-screen/WorkoutCard.js
@@ -14,80 +14,67 @@ import shortInterval from "../../static/short-interval.png"
 import longInterval from "../../static/long-interval.png"
 
 const WorkoutCard = ({workout, onToggle}) => {
+    // This component owns the data and display of each daily workout
     const [instructions, setInstructions] = useState([]);
     const [icon, setIcon] = useState("");
 
-    let all_phases = [];
+    function getPhasesWithZoneCSS(){
+        // Turns the Zone words (e.g. "Zone 1") into CSS circles to illustrate the heart rate zone
+        // Without these circles the workout instructions become too long to fit within a card
+        let phases_with_css = [];
 
-    instructions.map((instruction) => {
-        let single_instruction = []
-
-        instruction.split(" ").map((word) => {
-            if (word.includes("Zone")) {
-                
-                word = word.replace("Zone", "")
-                const zone_num = word.at(0);
-                // This catches the ")" that sometimes appears as: "Zone {n})""
-                // TODO - remove? This var isn't used.
-                const remaining = word.slice(1,);
-
-                single_instruction.push(<><span className={"heart-rate-zone-small zone" + zone_num}>{zone_num}</span></>)
-            }
-            else {
-                single_instruction.push(<span>{word} </span>);
-            }
+        instructions.map((instruction) => {
+            let single_instruction = []
+            instruction.split(" ").map((word) => {
+                if (word.includes("Zone")) {
+                    word = word.replace("Zone", "")
+                    const zone_num = word.at(0);
+                    single_instruction.push(<><span className={"heart-rate-zone-small zone" + zone_num}>{zone_num}</span></>)
+                }
+                else {
+                    single_instruction.push(<span>{word} </span>);
+                }
+            });
+            phases_with_css.push(<p className="phase">{single_instruction}</p>);
         });
-        all_phases.push(<p className="phase">{single_instruction}</p>);
-    });
 
-    function getIcon(wrkt_title){
-        if(wrkt_title.includes("Foundation"))
-        {
-            return foundation;
-        }
-        else if(wrkt_title.includes("Hill Repetition"))
-        {
-            return hillRepetition;
-        }
-        else if(wrkt_title.includes("Speed Play"))
-        {
-            return speedPlay;
-        }
-        else if(wrkt_title.includes("Race"))
-        {
-            return race;
-        }
-        else if(wrkt_title.includes("Rest"))
-        {
-            return rest;
-        }
-        else if(wrkt_title.includes("Long Run"))
-        {
-            return longRun;
-        }
-        else if(wrkt_title.includes("Recovery"))
-        {
-            return recovery;
-        }
-        else if(wrkt_title.includes("Fast Finish"))
-        {
-            return fastFinish;
-        }
-        else if(wrkt_title.includes("Short Interval"))
-        {
-            return shortInterval;
-        }
-        else if(wrkt_title.includes("Long Interval"))
-        {
-            return longInterval;
+        return phases_with_css;
+    }
+
+    function getIcon(full_title){
+        // Match the text part in the workout name, e.g. "Foundation" in "Foundation 6"
+        // Then return the icon for this workout
+        let regex = /[a-zA-Z]/g;
+        let matches = full_title.match(regex);
+        let title = matches.join(""); 
+
+        switch(title) {
+            case "Foundation":
+                return foundation;
+            case "HillRepetition":
+                return hillRepetition;
+            case "SpeedPlay":
+                return speedPlay;
+            case "Race":
+                return race;
+            case "LongRun":
+                return longRun;
+            case "Recovery":
+                return recovery;
+            case "FastFinish":
+                return fastFinish;
+            case "ShortInterval":
+                return shortInterval;
+            case "LongInterval":
+                return longInterval;
+            case "Rest":
+                return rest;
         }
     }
 
-
-    // It would be better if the backend returned the workout instructions to reduce
-    // the amount of fetch requests between backend/frontend (now one per day) -  
-    // but more fun to do it in React :-)
-    // TODO - update, this approach is visibly slow in the UI. Refactor.
+    // For performance, it would be better if the backend returned the workout phases 
+    // as part of a prop. This would mean one request for all instructions vs. one request 
+    // for each workout card. But more fun to do it in React :-)
     useEffect(() => {        
         fetch("/workout-phases/" + workout.title)
         .then((response) => response.json())
@@ -96,15 +83,17 @@ const WorkoutCard = ({workout, onToggle}) => {
         });
 
         let icon_data = getIcon(workout.title);
-        setIcon(icon_data);        
-      }, []);
+        setIcon(icon_data);
+    }, []);
+    
+    let phases_with_css = getPhasesWithZoneCSS();
 
     return(
         <div className={`workout-card ${workout.complete ? "complete": ""}`} onDoubleClick={() => onToggle(workout.id)}>
             <img className="workout-icon" src={icon}></img>
             <p className="workout-title"><strong>{workout.title}</strong></p>
             <div className="workout-phases">
-                {all_phases} 
+                {phases_with_css} 
             </div>
         </div>
     )
